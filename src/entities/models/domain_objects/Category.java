3e10d5c165c4a583cfae8bafc7acc97e6a87182d
Jack Grimes <flangojack@hotmail.com>
2020-08-10 23:00:37 +0100

    Slight reformatting of category code.

--- src/entities/models/domain_objects/Category.java
+++ src/entities/models/domain_objects/Category.java
@@ -12,11 +12,27 @@ public class Category {
 	private int category_position;
 
 	// Getters and Setters
-	public String getCategory_UUID() { return category_uuid; }
-	public void setCategory_uuid(String category_uuid) {this.category_uuid = category_uuid;}
-	public String getCategory_title() { return category_title; }
-	public void setCategory_title(String new_title) { this.category_title = category_title; }
-	public int getCategory_position() { return category_position; }
-	public void setCategory_position(int category_position) { this.category_position = category_position; }
+	public String getCategory_uuid() {
+		return category_uuid;
+	}
 
+	public void setCategory_uuid(String category_uuid) {
+		this.category_uuid = category_uuid;
+	}
+
+	public String getCategory_title() {
+		return category_title;
+	}
+
+	public void setCategory_title(String category_title) {
+		this.category_title = category_title;
+	}
+
+	public int getCategory_position() {
+		return category_position;
+	}
+
+	public void setCategory_position(int category_position) {
+		this.category_position = category_position;
+	}
 }
