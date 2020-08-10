91c70637590a764731e35601c9cded29b1924b7a
Jack Grimes <flangojack@hotmail.com>
2020-08-10 23:30:49 +0100

    Added data for Template Card domain object.

--- src/entities/models/domain_objects/card_data/TemplateCard.java
+++ src/entities/models/domain_objects/card_data/TemplateCard.java
@@ -1,4 +1,25 @@
-package entities.models.card_data;
+package entities.models.domain_objects.card_data;
 
-public class TemplateCard extends Card {
+public class TemplateCard {
+
+	private String template_card_uuid;
+	private String template_card_title;
+
+
+	// Getters and Setters
+	public String getTemplate_card_uuid() {
+		return template_card_uuid;
+	}
+
+	public void setTemplate_card_uuid(String template_card_uuid) {
+		this.template_card_uuid = template_card_uuid;
+	}
+
+	public String getTemplate_card_title() {
+		return template_card_title;
+	}
+
+	public void setTemplate_card_title(String template_card_title) {
+		this.template_card_title = template_card_title;
+	}
 }
