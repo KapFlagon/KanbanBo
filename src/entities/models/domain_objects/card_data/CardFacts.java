1020dce5935ae592760fed61f63785c3540def9d
Jack Grimes <flangojack@hotmail.com>
2020-08-10 23:36:04 +0100

    Added domain object for card facts.

---
+++ src/entities/models/domain_objects/card_data/CardFacts.java
@@ -0,0 +1,25 @@
+package entities.models.domain_objects.card_data;
+
+public class CardFacts {
+
+	private String card_uuid;
+	private String card_description;
+
+
+	// Getters and Setters
+	public String getCard_uuid() {
+		return card_uuid;
+	}
+
+	public void setCard_uuid(String card_uuid) {
+		this.card_uuid = card_uuid;
+	}
+
+	public String getCard_description() {
+		return card_description;
+	}
+
+	public void setCard_description(String card_description) {
+		this.card_description = card_description;
+	}
+}
