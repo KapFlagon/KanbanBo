6ac004e51e67b2f2c104a7106426ed51274bc7b3
Jack Grimes <flangojack@hotmail.com>
2020-08-11 00:16:43 +0100

    Added data for domain object joiner class between Cards and Checklists.

---
+++ src/entities/models/domain_objects/card_data/CardChecklists.java
@@ -0,0 +1,25 @@
+package entities.models.domain_objects.card_data;
+
+public class CardChecklists {
+
+	private String parent_card_uuid;
+	private String linked_checklist_uuid;
+
+
+	// Getters and Setters
+	public String getParent_card_uuid() {
+		return parent_card_uuid;
+	}
+
+	public void setParent_card_uuid(String parent_card_uuid) {
+		this.parent_card_uuid = parent_card_uuid;
+	}
+
+	public String getLinked_checklist_uuid() {
+		return linked_checklist_uuid;
+	}
+
+	public void setLinked_checklist_uuid(String linked_checklist_uuid) {
+		this.linked_checklist_uuid = linked_checklist_uuid;
+	}
+}
