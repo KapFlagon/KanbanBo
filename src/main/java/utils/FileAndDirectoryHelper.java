0fa255a04833728943b37f1f7cd22c6d1edd66b9
Jack Grimes <flangojack@hotmail.com>
2020-08-24 11:56:48 +0100

    Helper class for checking files and directories, and creating them.

---
+++ src/main/java/utils/FileAndDirectoryHelper.java
@@ -0,0 +1,43 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class FileAndDirectoryHelper {
+
+
+	private static boolean pathItemExists(Path itemPath) {
+		File itemTester = itemPath.toFile();
+		return itemTester.exists();
+	}
+
+	public static boolean fileExists(Path filePath) {
+		if(pathItemExists(filePath)) {
+			File fileTester = filePath.toFile();
+			return fileTester.isFile();
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean directoryExists(Path directoryPath) {
+		if(pathItemExists(directoryPath)) {
+			File directoryTester = directoryPath.toFile();
+			return directoryTester.isDirectory();
+		} else {
+			return false;
+		}
+	}
+
+	public static void createFile(Path path) throws IOException {
+		File file = path.toFile();
+		file.createNewFile();
+	}
+
+	public static void createDirectory(Path path) throws IOException {
+		File file = path.toFile();
+		file.mkdirs();
+	}
+
+}
