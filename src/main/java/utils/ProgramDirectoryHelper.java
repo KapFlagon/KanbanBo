220f6de82a3b9c28c072da508e861b7298feb4ad
Jack Grimes <flangojack@hotmail.com>
2020-08-25 18:25:33 +0100

    Converted methods in ProgramDirectoryHelper class to static methods for path retrieval.

--- src/main/java/utils/ProgramDirectoryHelper.java
+++ src/main/java/utils/ProgramDirectoryHelper.java
@@ -10,48 +10,14 @@ public class ProgramDirectoryHelper {
 	// https://stackoverflow.com/questions/320542/how-to-get-the-path-of-a-running-jar-file
 
 	// Variables
-	private Path classFilePath;
-	private Path executedDirectoryPath;
-	private Path cssFolderPath;
-	private Path propertiesFilePath;
 
 
 	// Constructors
-	public ProgramDirectoryHelper() throws URISyntaxException, IOException {
-		classFilePath = Paths.get(ProgramDirectoryHelper.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-		//System.out.println("classFilePath: " + classFilePath.toString());
-		parseProgramDirectory();
-		//System.out.println("executedDirectoryPath: " + executedDirectoryPath);
-		parseCssFolderPath();
-		//System.out.println("cssFolderPath: " + cssFolderPath);
-		parsePropertiesPath();
-		//System.out.println("propertiesFilePath: " + propertiesFilePath);
-	}
 
-
-	// Getters and Setters
-	public Path getClassFilePath() {
-		return classFilePath;
-	}
-
-	public Path getExecutedDirectoryPath() {
-		return executedDirectoryPath;
-	}
-
-	public Path getCssFolderPath() {
-		return cssFolderPath;
-	}
-
-	public Path getPropertiesFilePath() {
-		return propertiesFilePath;
-	}
-
-
-	// Initialisation methods
-
-
+	
 	// Other methods
-	private boolean parseProgramDirectory() {
+	private static Path parseProgramDirectory() throws URISyntaxException {
+		Path classFilePath = classFilePath = Paths.get(ProgramDirectoryHelper.class.getProtectionDomain().getCodeSource().getLocation().toURI());
 		String classFileUrlString =  classFilePath.toString();
 		String parentFolderString;
 		if(classIsInJar(classFileUrlString)) {
@@ -61,23 +27,23 @@ public class ProgramDirectoryHelper {
 			int indexOfFileName = classFileUrlString.lastIndexOf("KanbanBo");
 			parentFolderString = classFileUrlString.substring(0, indexOfFileName);
 		}
-		//System.out.println("parent folder string: " + parentFolderString);
-		executedDirectoryPath = Paths.get(parentFolderString);
-		return FileAndDirectoryHelper.directoryExists(executedDirectoryPath);
+		Path executedDirectoryPath = Paths.get(parentFolderString);
+		return executedDirectoryPath;
 	}
 
-	private void parseCssFolderPath() throws IOException {
-		cssFolderPath = Paths.get(executedDirectoryPath.toString(), "styles");
+	public static Path parseCssFolderPath() throws URISyntaxException {
+		Path executedDirectoryPath = parseProgramDirectory();
+		Path cssFolderPath = Paths.get(executedDirectoryPath.toString(), "styles");
+		return cssFolderPath;
 	}
 
-	private void parsePropertiesPath() throws IOException {
-		propertiesFilePath = Paths.get(executedDirectoryPath.toString(), "KanbanBo.properties");
-		if(!FileAndDirectoryHelper.fileExists(propertiesFilePath)) {
-			FileAndDirectoryHelper.createFile(propertiesFilePath);
-		}
+	public static Path  parsePropertiesPath() throws URISyntaxException {
+		Path executedDirectoryPath =parseProgramDirectory();
+		Path propertiesFilePath = Paths.get(executedDirectoryPath.toString(), "KanbanBo.properties");
+		return propertiesFilePath;
 	}
 
-	private boolean classIsInJar(String filePath) {
+	private static boolean classIsInJar(String filePath) {
 		int indexOfFileName = filePath.lastIndexOf(".jar");
 		if(indexOfFileName < 1) {
 			return false;
