96a65f65008410253adfb3073f6aa49c1e1e3cdd
Jack Grimes <flangojack@hotmail.com>
2020-09-07 13:34:48 +0100

    Added ORMLite annotations to active board domain object.

--- src/main/java/entities/models/domain_objects/board_data/active_board/ActiveBoard.java
+++ src/main/java/entities/models/domain_objects/board_data/active_board/ActiveBoard.java
@@ -1,11 +1,20 @@
 package entities.models.domain_objects.board_data.active_board;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "active_board")
 public class ActiveBoard {
 
+	@DatabaseField(id = true, canBeNull = false, unique = true, useGetSet = true)
 	private String active_board_uuid;
+	@DatabaseField(canBeNull = false, useGetSet = true, foreign = true)
 	private String category_uuid;
+	@DatabaseField(canBeNull = false, useGetSet = true)
 	private String active_board_title;
+	@DatabaseField(canBeNull = false, useGetSet = true)
 	private int active_board_position;
+	@DatabaseField(canBeNull = false, useGetSet = true)
 	private boolean active_board_favourite;
 
 
