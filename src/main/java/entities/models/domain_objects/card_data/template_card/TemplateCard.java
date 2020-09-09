c0c415b5ee9ed02896e35934c7749b122b133ddd
Jack Grimes <flangojack@hotmail.com>
2020-09-09 19:48:57 +0100

    Adding ORMLite annotations for data persistence in class.

--- src/main/java/entities/models/domain_objects/card_data/template_card/TemplateCard.java
+++ src/main/java/entities/models/domain_objects/card_data/template_card/TemplateCard.java
@@ -1,8 +1,14 @@
 package entities.models.domain_objects.card_data.template_card;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "template_card")
 public class TemplateCard {
 
+	@DatabaseField(id = true, canBeNull = false, unique = true, useGetSet = true)
 	private String template_card_uuid;
+	@DatabaseField(canBeNull = false, useGetSet = true)
 	private String template_card_title;
 
 
