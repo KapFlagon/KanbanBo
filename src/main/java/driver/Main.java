1df9e49b4dbaafbb370d2c899af962d2afc0e5cc
Jack Grimes <flangojack@hotmail.com>
2021-03-05 14:08:35 +0000

    Adding a 'Main' class so that it resolves a JavaFX bug, by acting as the main method entry point of the program.

---
+++ src/main/java/driver/Main.java
@@ -0,0 +1,9 @@
+package driver;
+
+public class Main {
+
+    // Dummy driver to bypass JavaFX 11 issues when creating JARs
+    public static void main(String[] args) {
+        KanbanBoApp.main(args);
+    }
+}
