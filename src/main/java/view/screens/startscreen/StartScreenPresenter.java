f669b5ae4bfd3e0e9927cb8a75a680845406865e
Jack Grimes <flangojack@hotmail.com>
2021-03-08 18:21:40 +0000

    Added private encapsulation with getters and setters for the injected fxml nodes in the view. Updated some System.out.println messages for temporary logging. Added a method to change scene to the Main Screen. Added references to the File creation class for actual file creation/deletion, etc.

--- src/main/java/view/screens/startscreen/StartScreenPresenter.java
+++ src/main/java/view/screens/startscreen/StartScreenPresenter.java
@@ -5,79 +5,178 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import utils.DatabaseUtils;
 import utils.FileChooserUtils;
+import utils.FileCreationUtils;
+import utils.StageUtils;
+import view.screens.mainscreen.MainScreenPresenter;
+import view.screens.mainscreen.MainScreenView;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StartScreenPresenter implements Initializable {
 
-    // Variables
+    // FXML injected variables
     @FXML
-    Button newDbButton;
+    private BorderPane borderPane;
+    @FXML
+    private Button newDbButton;
+    @FXML
+    private Button openDbButton;
+    @FXML
+    private MenuItem newDbMenuItem;
+    @FXML
+    private MenuItem openDbMenuItem;
+    @FXML
+    private MenuItem exitMenuItem;
+    @FXML
+    private MenuItem aboutMenuItem;
+    @FXML
+    private MenuItem repoMenuItem;
 
-    @FXML
-    Button openDbButton;
+    // Other variables
 
-    @FXML
-    MenuItem newDbMenuItem;
-
-    @FXML
-    MenuItem openDbMenuItem;
-
-    @FXML
-    MenuItem exitMenuItem;
-
-    @FXML
-    MenuItem aboutMenuItem;
-
-    @FXML
-    MenuItem repoMenuItem;
 
     // Constructors
 
 
     // Getters and Setters
+    public BorderPane getBorderPane() {
+        return borderPane;
+    }
 
+    public void setBorderPane(BorderPane borderPane) {
+        this.borderPane = borderPane;
+    }
+
+    public Button getNewDbButton() {
+        return newDbButton;
+    }
+
+    public void setNewDbButton(Button newDbButton) {
+        this.newDbButton = newDbButton;
+    }
+
+    public Button getOpenDbButton() {
+        return openDbButton;
+    }
+
+    public void setOpenDbButton(Button openDbButton) {
+        this.openDbButton = openDbButton;
+    }
+
+    public MenuItem getNewDbMenuItem() {
+        return newDbMenuItem;
+    }
+
+    public void setNewDbMenuItem(MenuItem newDbMenuItem) {
+        this.newDbMenuItem = newDbMenuItem;
+    }
+
+    public MenuItem getOpenDbMenuItem() {
+        return openDbMenuItem;
+    }
+
+    public void setOpenDbMenuItem(MenuItem openDbMenuItem) {
+        this.openDbMenuItem = openDbMenuItem;
+    }
+
+    public MenuItem getExitMenuItem() {
+        return exitMenuItem;
+    }
+
+    public void setExitMenuItem(MenuItem exitMenuItem) {
+        this.exitMenuItem = exitMenuItem;
+    }
+
+    public MenuItem getAboutMenuItem() {
+        return aboutMenuItem;
+    }
+
+    public void setAboutMenuItem(MenuItem aboutMenuItem) {
+        this.aboutMenuItem = aboutMenuItem;
+    }
+
+    public MenuItem getRepoMenuItem() {
+        return repoMenuItem;
+    }
+
+    public void setRepoMenuItem(MenuItem repoMenuItem) {
+        this.repoMenuItem = repoMenuItem;
+    }
 
 
     // Initialisation methods
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        StageUtils.getMainStage().setTitle("KanbanBo - Database file selection");
+        System.out.println("Start screen loaded");
     }
 
 
     // Other methods
-    public void createDbFile() {
+    public void createDbFile() throws IOException {
         System.out.println("creating");
-        File createdFile = FileChooserUtils.createFilePopup();
-        // TODO Insert some kind of logging for creating file here.
+        File newFile = FileChooserUtils.createFilePopup();
+        if(newFile != null) {
+            FileCreationUtils.createEmptyDatabaseFile(newFile);
+            DatabaseUtils.setActiveDatabaseFile(newFile);
+            System.out.println("DatabaseUtils updated to: " + DatabaseUtils.getActiveDatabaseFile().toString());
+            moveToMainSceneView();
+        } else {
+            // Accounting for scenario where user cancels file creation.
+            System.out.println("File creation cancelled");
+        }
+        // TODO Insert some kind of logging here.
     }
 
 
     public void openDbFile() {
         System.out.println("opening");
         File selectedFile = FileChooserUtils.openFilePopup();
+        if (selectedFile != null) {
+            // Accounting for scenario where user cancels opening file.
+            DatabaseUtils.setActiveDatabaseFile(selectedFile);
+            System.out.println("DatabaseUtils updated to: " + DatabaseUtils.getActiveDatabaseFile().toString());
+            moveToMainSceneView();
+        } else {
+            System.out.println("File opening cancelled");
+        }
         // TODO Insert some kind of logging for selected file here.
     }
 
 
     public void exitApplication() {
-        System.out.println("exiting");
+        System.out.println("Exiting application");
         Platform.exit();
         System.exit(0);
     }
 
 
     public void displayApplicationInfo() {
-        System.out.println("Info");
+        System.out.println("Info output");
     }
 
 
     public void openOnlineCodeRepo() {
-        System.out.println("repo");
+        System.out.println("Redirect to online repository");
+        //Desktop dt = Desktop.getDesktop();
+        //try {
+        //    dt.browse(new URI("http://www.github.com"));
+        //} catch (Exception exception) {
+        //    exception.printStackTrace();
+        //}
+    }
+
+    private void moveToMainSceneView() {
+        //MainScreenView mainScreenView = new MainScreenView();
+        MainScreenView view = new MainScreenView();
+        MainScreenPresenter presenter = (MainScreenPresenter) view.getPresenter();
+        StageUtils.changeScene("KanbanBo - Project manager", view);
     }
 
 }
