c886a4f057cc82139172ef801125eb41a6448f13
Jack Grimes <flangojack@hotmail.com>
2021-03-08 19:45:07 +0000

    Adding some placeholder afterburner elements for subviews for database recent item list in the start screen.

---
+++ src/main/java/view/screens/startscreen/subviews/recentdbfileitemview/RecentDbFilesItemView.java
@@ -0,0 +1,7 @@
+package view.screens.startscreen.subviews.recentdbfileitemview;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class RecentDbFilesItemView extends FXMLView {
+    // Keep empty for afterburner.fx default implementation
+}
---
+++ src/main/java/view/screens/startscreen/subviews/recentdbfilesview/RecentDbFilesView.java
@@ -0,0 +1,7 @@
+package view.screens.startscreen.subviews.recentdbfilesview;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class RecentDbFilesView extends FXMLView {
+    // Keep empty for afterburner.fx default implementation
+}
