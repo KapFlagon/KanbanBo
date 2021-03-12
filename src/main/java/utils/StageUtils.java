39cadbda6479be18271195efff7a3ac1355acfe9
Jack Grimes <flangojack@hotmail.com>
2021-03-12 21:47:37 +0000

    Added some 'sub-stage' structures for handling child pop-ups etc. later.

--- src/main/java/utils/StageUtils.java
+++ src/main/java/utils/StageUtils.java
@@ -1,26 +1,73 @@
 package utils;
 
 import com.airhacks.afterburner.views.FXMLView;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import java.util.ArrayDeque;
 
 public class StageUtils {
 
-    // Static reference to main stage to allow pop-ups.
-    private static Stage mainStage;
+    // Variables
+    private static Stage mainStage;         // Static reference to main stage to allow pop-ups.
+    private static ArrayDeque<Stage> subStages;  // Queue of all sub-stages to be processed in order
 
+
+    // Getters and Setters
     public static Stage getMainStage() {
         return mainStage;
     }
-
     public static void setMainStage(Stage newMainStage) {
         StageUtils.mainStage = newMainStage;
     }
 
-    public static void changeScene(String newTitle, FXMLView newView) {
+    public static ArrayDeque<Stage> getSubStages() {
+        return subStages;
+    }
+    public static void setSubStages(ArrayDeque<Stage> subStages) {
+        StageUtils.subStages = subStages;
+    }
+
+    // Other methods
+    public static void changeMainScene(String newTitle, FXMLView newView) {
         Scene newViewScene = new Scene(newView.getView());
-        getMainStage().setTitle(newTitle);
-        getMainStage().setScene(newViewScene);
+        StageUtils.getMainStage().setTitle(newTitle);
+        StageUtils.getMainStage().setScene(newViewScene);
+    }
+
+    public static void addSubStageToDeque(Stage subStage) {
+        StageUtils.getSubStages().addLast(subStage);
+    }
+
+    public static void removeSubStageFromDeque(Stage subStage) {
+        StageUtils.getSubStages().remove(subStage);
+    }
+
+    public static void removeLastSubStageFromDeque() {
+        StageUtils.getSubStages().removeLast();
+    }
+
+    public static void createChildStage(String title, Parent parent) {
+        if (StageUtils.getSubStages() == null) {
+            StageUtils.setSubStages(new ArrayDeque<Stage>());
+        }
+        Stage childStage = new Stage();
+        childStage.setTitle(title);
+        childStage.initOwner(StageUtils.getMainStage());
+        childStage.initModality(Modality.APPLICATION_MODAL);
+        Scene internalScene = new Scene(parent);
+        childStage.setScene(internalScene);
+        StageUtils.addSubStageToDeque(childStage);
+    }
+
+    public static void showSubStage() {
+        StageUtils.getSubStages().peekLast().show();
+    }
+
+    public static void closeSubStage() {
+        StageUtils.getSubStages().peekLast().close();
+        StageUtils.removeLastSubStageFromDeque();
     }
 
 }
