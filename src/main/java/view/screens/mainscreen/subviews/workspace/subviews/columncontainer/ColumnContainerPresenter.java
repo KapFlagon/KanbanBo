b99c18ec9cd68a754f0375f61fe6c7dd95c6e5b0
Jack Grimes <flangojack@hotmail.com>
2021-03-30 20:21:15 +0100

    Added an empty container for displaying card data in the Kanban board for processing.

---
+++ src/main/java/view/screens/mainscreen/subviews/workspace/subviews/columncontainer/ColumnContainerPresenter.java
@@ -0,0 +1,29 @@
+package view.screens.mainscreen.subviews.workspace.subviews.columncontainer;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ColumnContainerPresenter implements Initializable {
+
+    // JavaFX injected node variables
+
+    // Other variables
+
+    // Constructors
+
+    // Getters & Setters
+
+    // Initialization methods
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    // UI event methods
+
+    // Other methods
+
+
+}
---
+++ src/main/java/view/screens/mainscreen/subviews/workspace/subviews/columncontainer/ColumnContainerView.java
@@ -0,0 +1,7 @@
+package view.screens.mainscreen.subviews.workspace.subviews.columncontainer;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class ColumnContainerView extends FXMLView {
+    // Keep empty for afterburner.fx default implementation
+}
