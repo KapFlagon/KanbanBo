e9d5a1179c13bdfb333974284b4202c53c7c4c40
Jack Grimes <flangojack@hotmail.com>
2021-03-31 20:56:40 +0100

    Updated the column presenter class so it can be empty added to the project view in the workspace.

--- src/main/java/view/screens/mainscreen/subviews/workspace/subviews/columncontainer/ColumnContainerPresenter.java
+++ src/main/java/view/screens/mainscreen/subviews/workspace/subviews/columncontainer/ColumnContainerPresenter.java
@@ -1,6 +1,12 @@
 package view.screens.mainscreen.subviews.workspace.subviews.columncontainer;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,8 +14,18 @@ import java.util.ResourceBundle;
 public class ColumnContainerPresenter implements Initializable {
 
     // JavaFX injected node variables
+    @FXML
+    private VBox cardVBox;
+    @FXML
+    private Label columnTitleLbl;
+    @FXML
+    private Text cardCountTxt;
+    @FXML
+    private Button addCardBtn;
+
 
     // Other variables
+    private ObservableList cards;
 
     // Constructors
 
@@ -22,6 +38,9 @@ public class ColumnContainerPresenter implements Initializable {
     }
 
     // UI event methods
+    public void addCard() {
+        System.out.println("Adding card");
+    }
 
     // Other methods
 
