b329fe7a51b2d731a09e9e6bc8b6af8e01de00ff
Jack Grimes <flangojack@hotmail.com>
2021-03-31 20:56:05 +0100

    Adding some code to get ready to add cards to the columns. Added some code to add non-filled columns to the view.

--- src/main/java/view/screens/mainscreen/subviews/workspace/subviews/projectcontainer/ProjectContainerPresenter.java
+++ src/main/java/view/screens/mainscreen/subviews/workspace/subviews/projectcontainer/ProjectContainerPresenter.java
@@ -4,8 +4,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import model.activerecords.ProjectActiveRecord;
 import model.domainobjects.project.ActiveProjectModel;
+import view.screens.mainscreen.subviews.workspace.subviews.columncontainer.ColumnContainerPresenter;
+import view.screens.mainscreen.subviews.workspace.subviews.columncontainer.ColumnContainerView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +20,8 @@ public class ProjectContainerPresenter implements Initializable {
     private Label projectTitleLbl;
     @FXML
     private Button createColumnBtn;
+    @FXML
+    private HBox columnHBox;
 
     // Other variables
     //private AbstractProjectModel projectModel;
@@ -56,11 +61,13 @@ public class ProjectContainerPresenter implements Initializable {
     // UI event methods
     public void createColumn() {
         System.out.println("Create Column...");
+        ColumnContainerView ccv = new ColumnContainerView();
+        ColumnContainerPresenter ccp = (ColumnContainerPresenter) ccv.getPresenter();
+        // use ccp to set data in the column data.
+        columnHBox.getChildren().add(ccv.getView());
     }
 
     // Other methods
-    private void doTheThings() {
 
-    }
 
 }
