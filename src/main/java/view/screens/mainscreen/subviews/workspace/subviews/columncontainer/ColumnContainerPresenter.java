9f0a24e790a88610797f8123d955eb363adf32b0
Jack Grimes <flangojack@hotmail.com>
2021-04-02 11:15:15 +0100

    Added a reference to the new column repository service.

--- src/main/java/view/screens/mainscreen/subviews/workspace/subviews/columncontainer/ColumnContainerPresenter.java
+++ src/main/java/view/screens/mainscreen/subviews/workspace/subviews/columncontainer/ColumnContainerPresenter.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import model.activerecords.ProjectColumnActiveRecord;
 import model.domainobjects.column.ActiveProjectColumnModel;
+import model.repositories.services.ProjectColumnRepositoryService;
 
 import java.net.URL;
 import java.util.ResourceBundle;
