3bb4d4b32ef265a2c759e31d0ee612c165f1cae7
Jack Grimes <flangojack@hotmail.com>
2021-04-09 14:47:05 +0100

    Deleted some unnecessary files.

--- src/main/java/view/screens/startscreen/subviews/recentdbfileitemview/RecentDbFilesItemView.java
+++
@@ -1,7 +0,0 @@
-package view.screens.startscreen.subviews.recentdbfileitemview;
-
-import com.airhacks.afterburner.views.FXMLView;
-
-public class RecentDbFilesItemView extends FXMLView {
-    // Keep empty for afterburner.fx default implementation
-}
--- src/main/java/view/screens/startscreen/subviews/recentdbfilesview/RecentDbFilesView.java
+++
@@ -1,7 +0,0 @@
-package view.screens.startscreen.subviews.recentdbfilesview;
-
-import com.airhacks.afterburner.views.FXMLView;
-
-public class RecentDbFilesView extends FXMLView {
-    // Keep empty for afterburner.fx default implementation
-}
