d2ac6e8dae15eecc9aff423fbba6fd2f9b0e854b
Jack Grimes <flangojack@hotmail.com>
2021-04-11 18:05:51 +0100

    Added a static 'foreign key' accessor for the parent column, to get access to the uuid.

--- src/main/java/model/domainobjects/card/ActiveColumnCardModel.java
+++ src/main/java/model/domainobjects/card/ActiveColumnCardModel.java
@@ -9,12 +9,14 @@ import java.util.UUID;
 @DatabaseTable(tableName = "active_column_card")
 public class ActiveColumnCardModel extends AbstractColumnCardModel{
 
+    // Constant for query building using database fields
+    public static final String FOREIGN_KEY_NAME = "parent_column_uuid";
 
     // Variables
     @DatabaseField(canBeNull = false, useGetSet = true, dataType = DataType.INTEGER)
     private int card_position;
-    @DatabaseField(canBeNull = false, useGetSet = true)
-    private UUID parent_column;
+    @DatabaseField(canBeNull = false, dataType = DataType.UUID, useGetSet = true, columnName = FOREIGN_KEY_NAME)
+    private UUID parent_column_uuid;
 
     // Constructors
 
@@ -27,11 +29,11 @@ public class ActiveColumnCardModel extends AbstractColumnCardModel{
         this.card_position = card_position;
     }
 
-    public UUID getParent_column() {
-        return parent_column;
+    public UUID getParent_column_uuid() {
+        return parent_column_uuid;
     }
-    public void setParent_column(UUID parent_column) {
-        this.parent_column = parent_column;
+    public void setParent_column_uuid(UUID parent_column_uuid) {
+        this.parent_column_uuid = parent_column_uuid;
     }
 
     // Initialisation methods
