7b21b24df60bf988fd899fabda5b2f338938edb7
Jack Grimes <flangojack@hotmail.com>
2021-04-26 22:41:21 +0100

    Updating the status model with getters and setters.

--- src/main/java/model/domainobjects/StatusModel.java
+++ src/main/java/model/domainobjects/StatusModel.java
@@ -22,6 +22,19 @@ public class StatusModel {
     }
 
     // Getters and Setters
+    public int getStatus_id() {
+        return status_id;
+    }
+    public void setStatus_id(int status_id) {
+        this.status_id = status_id;
+    }
+
+    public String getStatus_text() {
+        return status_text;
+    }
+    public void setStatus_text(String status_text) {
+        this.status_text = status_text;
+    }
 
 
     // Initialisation methods
