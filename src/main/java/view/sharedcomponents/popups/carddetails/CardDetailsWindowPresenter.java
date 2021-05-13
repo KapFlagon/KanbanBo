9e4ce3132cbdff929bdf2e6345eeb0bc8398d317
Jack Grimes <flangojack@hotmail.com>
2021-05-13 21:18:41 +0100

    Adding some basic input validation for the card data pop-up.

--- src/main/java/view/sharedcomponents/popups/carddetails/CardDetailsWindowPresenter.java
+++ src/main/java/view/sharedcomponents/popups/carddetails/CardDetailsWindowPresenter.java
@@ -2,8 +2,10 @@ package view.sharedcomponents.popups.carddetails;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import model.activerecords.ColumnCardActiveRecord;
 import model.activerecords.ProjectColumnActiveRecord;
 import model.domainobjects.card.CardModel;
@@ -21,9 +23,13 @@ public class CardDetailsWindowPresenter implements Initializable {
     @FXML
     private TextField titleTextField;
     @FXML
+    private Label titleErrorLbl;
+    @FXML
     private TextArea descriptionText;
 
     // Other variables
+    private String noTitleError = "A title must be provided";
+    private boolean validTitle;
     private ProjectColumnActiveRecord<ColumnModel> parentColumnActiveRecord;
     private ColumnCardActiveRecord<CardModel> columnCardActiveRecord;
 
@@ -49,20 +55,29 @@ public class CardDetailsWindowPresenter implements Initializable {
     // Initialization methods
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        validTitle = true;
+        titleErrorLbl.setText(noTitleError);
+        titleErrorLbl.setVisible(false);
+        titleErrorLbl.setDisable(true);
+        establishTitleTextFieldValidation();
     }
 
     // UI event methods
     public void saveDetailsChange() throws IOException, SQLException {
-        if(columnCardActiveRecord == null) {
-            columnCardActiveRecord = new ColumnCardActiveRecord<>(CardModel.class);
-            // TODO make new project building the responsibility of class ProjectActiveRecord
-            columnCardActiveRecord.setColumnCardModel(buildNewCardModelInstance());
+        if(validTitle) {
+            if(columnCardActiveRecord == null) {
+                columnCardActiveRecord = new ColumnCardActiveRecord<>(CardModel.class);
+                // TODO make new project building the responsibility of class ProjectActiveRecord
+                columnCardActiveRecord.setColumnCardModel(buildNewCardModelInstance());
+            } else {
+                columnCardActiveRecord.setCardTitle(titleTextField.getText());
+                columnCardActiveRecord.setCardDescription(descriptionText.getText());
+            }
+            StageUtils.hideSubStage();
         } else {
-            columnCardActiveRecord.setCardTitle(titleTextField.getText());
-            columnCardActiveRecord.setCardDescription(descriptionText.getText());
+            titleErrorLbl.setVisible(true);
+            titleErrorLbl.setDisable(false);
         }
-        StageUtils.hideSubStage();
     }
 
 
@@ -79,6 +94,18 @@ public class CardDetailsWindowPresenter implements Initializable {
         cardModel.setCard_description_text(descriptionText.getText());
         cardModel.setParent_column_uuid(parentColumnActiveRecord.getProjectColumnModel().getColumn_uuid());
         return cardModel;
+    }
+
+    private void establishTitleTextFieldValidation() {
+        titleTextField.setTextFormatter(new TextFormatter<Object> (change -> {
+            change = change.getControlNewText().matches(".{0,50}") ? change : null;
+            if (change != null && change.getControlNewText().length() < 1) {
+                validTitle = false;
+            } else {
+                validTitle = true;
+            }
+            return change;
+        }));
     }
 
 }
