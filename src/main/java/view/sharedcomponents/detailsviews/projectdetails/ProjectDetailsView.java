7f0403acd32c9c54987281f87b1462b345afd918
Jack Grimes <flangojack@hotmail.com>
2021-07-13 20:57:15 +0100

    Starting a basic implementation idea for a 'details' view which will animate out from the side screen when new projects are selected for creation. To replace the pop-up that currently exists.

---
+++ src/main/java/view/sharedcomponents/detailsviews/projectdetails/ProjectDetailsView.java
@@ -0,0 +1,7 @@
+package view.sharedcomponents.detailsviews.projectdetails;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class ProjectDetailsView extends FXMLView {
+    // Keep empty for afterburner.fx default implementation
+}
