f65dbffbe35de553c6c0f2a13940fe0091a504fc
Jack Grimes <flangojack@hotmail.com>
2021-08-08 15:53:38 +0100

    Adding a modular confirmation dialog pop-up.

---
+++ src/main/java/view/sharedcomponents/popups/confirmationdialog/ConfirmationDialogView.java
@@ -0,0 +1,7 @@
+package view.sharedcomponents.popups.confirmationdialog;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class ConfirmationDialogView extends FXMLView {
+    // Keep empty for afterburner.fx default implementation
+}
