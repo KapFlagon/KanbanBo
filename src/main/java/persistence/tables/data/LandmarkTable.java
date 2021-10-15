07f2e0049bfce451c44939102725f6a075545b23
Jack Grimes <flangojack@hotmail.com>
2021-10-15 12:53:56 +0100

    Adding a new table for 'landmark' concept, to be implemented into business logic later.

---
+++ src/main/java/persistence/tables/data/LandmarkTable.java
@@ -0,0 +1,29 @@
+package persistence.tables.data;
+
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.UUID;
+
+@DatabaseTable(tableName = "landmark")
+public class LandmarkTable {
+
+
+    // Variables
+    private UUID landmark_uuid;
+    private String landmark_title;
+    private String landmark_description;
+
+
+    // Constructors
+
+
+    // Getters and Setters
+
+
+    // Initialisation methods
+
+
+    // Other methods
+
+
+}
