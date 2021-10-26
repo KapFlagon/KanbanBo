0973e86058f85bebbeb76235b77448cb4ad8203c
Jack Grimes <flangojack@hotmail.com>
2021-10-26 21:28:53 +0100

    Deleting unecessary card container code.

--- src/main/java/view/screens/mainscreen/subviews/workspace/subviews/cardcontainer/CardContainerView.java
+++
@@ -1,8 +0,0 @@
-package view.screens.mainscreen.subviews.workspace.subviews.cardcontainer;
-
-import com.airhacks.afterburner.views.FXMLView;
-import framework.CustomFXMLView;
-
-public class CardContainerView extends CustomFXMLView {
-    // Keep empty for afterburner.fx default implementation
-}
