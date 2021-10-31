6fc1fa71030c4089b41490aba37d147631ef6aca
Jack Grimes <flangojack@hotmail.com>
2021-10-31 18:01:43 +0000

    Adding a dialog for moving columns within a project.

---
+++ src/main/java/view/sharedviewcomponents/popups/movecolumndialog/MoveColumnDialogPresenter.java
@@ -0,0 +1,84 @@
+package view.sharedviewcomponents.popups.movecolumndialog;
+
+import domain.entities.column.ObservableColumn;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.util.StringConverter;
+import persistence.dto.column.ColumnDTO;
+import persistence.mappers.ObservableObjectToDTO;
+import persistence.services.KanbanBoDataService;
+import utils.StageUtils;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MoveColumnDialogPresenter implements Initializable {
+
+    // JavaFX injected node variables
+    @FXML
+    private Label columnTitleLbl;
+    @FXML
+    private Label finalColumnLbl;
+    @FXML
+    private ChoiceBox<Integer> columnPositionChoiceBox;
+
+    // Other variables
+    @Inject
+    KanbanBoDataService kanbanBoDataService;
+    private ObservableColumn columnToMove;
+
+    // Constructors
+
+    // Getters & Setters
+    public void setColumnToMove(ObservableColumn columnToMove) throws SQLException, IOException {
+        this.columnToMove = columnToMove;
+        columnTitleLbl.setText(columnToMove.columnTitleProperty().getValue());
+        int size = kanbanBoDataService.getRelatedColumns(columnToMove).size();
+        for(int iterator = 0; iterator < size; iterator++) {
+            columnPositionChoiceBox.getItems().add(iterator + 1);
+        }
+        columnPositionChoiceBox.getSelectionModel().select(Integer.valueOf(this.columnToMove.columnPositionProperty().getValue() + 1));
+    }
+
+
+    // Initialization methods
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    // UI event methods
+    @FXML
+    private void cancelSelection(ActionEvent event) {
+        System.out.println("cancel");
+        StageUtils.hideSubStage();
+        event.consume();
+    }
+
+    @FXML
+    private void saveSelection(ActionEvent event) {
+        ColumnDTO columnDTO = ObservableObjectToDTO.mapColumnTableToColumnDTO(columnToMove);
+        columnDTO.setPosition(columnPositionChoiceBox.getSelectionModel().getSelectedItem() - 1);
+        try {
+            kanbanBoDataService.moveColumn(columnDTO, columnToMove);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        StageUtils.hideSubStage();
+    }
+
+    // Other methods
+
+
+
+}
---
+++ src/main/java/view/sharedviewcomponents/popups/movecolumndialog/MoveColumnDialogView.java
@@ -0,0 +1,8 @@
+package view.sharedviewcomponents.popups.movecolumndialog;
+
+import com.airhacks.afterburner.views.FXMLView;
+import framework.CustomFXMLView;
+
+public class MoveColumnDialogView extends CustomFXMLView {
+    // Keep empty for afterburner.fx default implementation
+}
