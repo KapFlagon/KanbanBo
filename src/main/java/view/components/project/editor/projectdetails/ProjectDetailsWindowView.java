deeda2902435a6626cc86245b69bbc2b236a1ad5
Jack Grimes <flangojack@hotmail.com>
2021-12-02 11:22:25 +0000

    Moving as part of project restructure. Also updated logic so that status changes are now properly updated in the back end.

---
+++ src/main/java/view/components/project/editor/projectdetails/ProjectDetailsWindowView.java
@@ -0,0 +1,7 @@
+package view.components.project.editor.projectdetails;
+
+import framework.CustomFXMLView;
+
+public class ProjectDetailsWindowView extends CustomFXMLView {
+    // Keep empty for afterburner.fx default implementation
+}
