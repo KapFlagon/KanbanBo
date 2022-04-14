f10bc770100f94040c327bca0791b6387ae1a908
Jack Grimes <flangojack@hotmail.com>
2022-04-14 07:55:52 +0100

    Updating AppInfo view with some translation texts and structure.

--- src/main/java/view/screens/sharedsubviews/appinfo/AppInfoPresenter.java
+++ src/main/java/view/screens/sharedsubviews/appinfo/AppInfoPresenter.java
@@ -1,16 +1,40 @@
 package view.screens.sharedsubviews.appinfo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextArea;
 
+import java.awt.*;
+import java.net.URI;
 import java.net.URL;
 import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class AppInfoPresenter implements Initializable {
 
     // JavaFX injected node variables
+    @FXML
+    private TextArea appFullLicenseTextArea;
+
+    @FXML
+    private Accordion fontsAccordion;
+
+    @FXML
+    private Accordion iconsAccordion;
+
+    @FXML
+    private Accordion imagesAndIllustrationsAccordion;
+
+    @FXML
+    private Accordion technologyAccordion;
+
 
     // Other variables
+    private String sourceCodeAddress;
     List< Map<String, String>> attributionEntryTextMapsList;
 
     // Constructors
@@ -43,9 +67,21 @@ public class AppInfoPresenter implements Initializable {
             }
         }
         System.out.println(attributionEntryTextMapsList);
+        sourceCodeAddress = resources.getString("hyperlink.sourcecode.target");
     }
 
     // UI event methods
+    @FXML
+    private void openSourceCodeLink(ActionEvent event) {
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.browse(new URI(sourceCodeAddress));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            // TODO Insert proper error handling response here.
+        }
+        event.consume();
+    }
 
     // Other methods
 
